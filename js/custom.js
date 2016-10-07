5f906d321d0fcf6d9b84ac227983429a01bd2832
selimdoyranli <selim@radkod.com>
2016-10-08 00:52:27 +0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function(){
+
+
+
+//Kaydol - Şifre Unuttum Linkleri Arası Geçiş
+$(document).ready(function(){
+$("#kayit-form").hide();
+$("#sifre-hatirlat-form").hide();	
+$(".hesap-olustur-link").click(function(e){
+$("#login-form").slideUp(0);	
+$("#kayit-form").fadeIn(300);	
+});
+
+$(".zaten-hesap-var-link").click(function(e){
+$("#kayit-form").slideUp(0);
+$("#sifre-hatirlat-form").slideUp(0);	
+$("#login-form").fadeIn(300);	
+});
+
+$(".sifre-hatirlat-link").click(function(e){
+$("#login-form").slideUp(0);	
+$("#sifre-hatirlat-form").fadeIn(300);	
+});
+});
+
+
+});
